122cf4e896980b78ea78623d1a4c907e6cee0be1
Seth Armistead <sdarmistead@gmail.com>
2025-10-24 14:25:23 +0200

    initiated project

---
+++ game.js
@@ -0,0 +1,9 @@
+let gamestopped = false
+
+
+
+while (!gamestopped) {
+
+  console.log('Hello there! Welcome to the world of Pokémon! This world is inhabited by creatures called Pokémon! For some people, Pokémon are pets. Other use them for fights.')
+  prompt('Right... So what is your name?')
+}
